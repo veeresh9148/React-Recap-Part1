3efab48dc0919470dd1df94adaa43ea8a63f018e
K <251634@NTTDATA.COM>
2022-05-16 12:15:37 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
+import './App.css'
+import FragmentDemo from './components/FragmentDemo'
+import Table from './components/Table'
+import RefsDemo from './components/RefsDemo'
+import FocusInput from './components/FocusInput'
+import FRParentInput from './components/FRParentInput'
+import PortalDemo from './components/PortalDemo'
+import Hero from './components/Hero'
+import ErrorBoundary from './components/ErrorBoundary'
+import ParentComp from './components/ParentComp'
+import ClickCounter from './components/ClickCounter'
+import HoverCounter from './components/HoverCounter'
+import Counter from './components/Counter'
+import ClickCounterTwo from './components/ClickCounterTwo'
+import HoverCounterTwo from './components/HoverCounterTwo'
+import ComponentC from './components/ComponentC'
+import { UserProvider } from './components/userContext'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	render() {
+		return (
+			<div className="App">
+					<ComponentC />
+				{/* <Counter
+					render={(count, incrementCount) =>
+					<ClickCounterTwo
+						count={count}
+						incrementCount={incrementCount}>
+					</ClickCounterTwo>}>
+				</Counter>
+				<Counter
+					render={(count, incrementCount) =>
+					<HoverCounterTwo
+						count={count}
+						incrementCount={incrementCount}>
+					</HoverCounterTwo>}>
+				</Counter> */}
+				{/* <ClickCounter name='Vishwas' /> */}
+				{/* <HoverCounter /> */}
+				{/* <ParentComp /> */}
+				{/* <Hero heroName="Batman" />
+				<Hero heroName="Superman" />
+				<ErrorBoundary>
+					<Hero heroName="Joker" />
+				</ErrorBoundary> */}
+			</div>
+		)
+	}
 }
 
-export default App;
+export default App
